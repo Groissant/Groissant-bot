156367d720ae386391c7d3fb97fc770b325bd2d8
Harry <xiaohanwu12@gmail.com>
2024-01-28 22:53:10 -0500

    fix: failed to parse key in base64

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ APP_ID = int(getenv('APP_ID') or 0)
 
 encoded_private_key = getenv('APP_PRIVATE_KEY')
 if encoded_private_key is not None:
-    APP_PRIVATE_KEY = str(base64.b64decode(encoded_private_key))
+    APP_PRIVATE_KEY = base64.b64decode(encoded_private_key).decode('utf-8')
 else:
     APP_PRIVATE_KEY = ""
 
